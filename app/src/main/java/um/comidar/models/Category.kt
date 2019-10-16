7b73a8cc32e56adf3ee5ff6c80566ec68bd4c62e
juanicaste <juanicastellano14@gmail.com>
2019-10-16 17:58:28 -0300

    images on categories

--- app/src/main/java/um/comidar/models/Category.kt
+++ app/src/main/java/um/comidar/models/Category.kt
@@ -3,4 +3,5 @@ package um.comidar.models
 import java.io.Serializable
 
 data class Category(val categoryId: Long = 0,
-                    val name: String): Serializable
+                    val name: String,
+                    val imageTemporaryUrl: String): Serializable
